9503228e15c1f8818f243b3df235861a641b0235
taha127 <tahadehghani127@gmail.com>
2025-03-20 20:05:09 +0330

    add custom user model and admin integration

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from accounts.forms import CustomUserChangeForm
+from accounts.models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserChangeForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
