02525c7d19f3d90d3fc1f5f085782c4e467a4d27
taha127 <tahadehghani127@gmail.com>
2025-03-23 17:07:29 +0330

    add password reset functionality with crispy forms integration and email configuration

--- config/settings.py
+++ config/settings.py
@@ -129,3 +129,5 @@ LOGOUT_REDIRECT_URL = 'home'
 # crispy forms settings
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
+# Email Config
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
