a234acfbc4e234bf87dc2d22949c98ba097f9d92
taha127 <tahadehghani127@gmail.com>
2025-03-24 00:03:08 +0330

    add book list template and update context object name in views

--- books/views.py
+++ books/views.py
@@ -3,10 +3,8 @@ from django.views import generic
 
 from .models import Book
 
+
 class BookListView(generic.ListView):
     model = Book
     template_name = 'books/book_list.html'
-
-
-
-
+    context_object_name = 'books'
