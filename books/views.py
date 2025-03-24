7b3117335334072377002bf65ecc35dff4c3cbab
taha127 <tahadehghani127@gmail.com>
2025-03-24 16:03:48 +0330

    add book update view, template, and update URLs for book editing

--- books/views.py
+++ books/views.py
@@ -19,3 +19,8 @@ class BookCreateView(generic.CreateView):
     model = Book
     fields = ['title', 'author', 'description', 'price']
     template_name = 'books/book_create.html'
+
+class BookUpdateView(generic.UpdateView):
+    model = Book
+    fields = ['title', 'author', 'description', 'price']
+    template_name = 'books/book_update.html'
