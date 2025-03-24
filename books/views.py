da663a5191a4a45bbcb9b7a20c9457096ed73f01
taha127 <tahadehghani127@gmail.com>
2025-03-24 16:27:40 +0330

    add book deletion view, template, and update URLs for book deletion

--- books/views.py
+++ books/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse_lazy
 
 from .models import Book
+
 
 
 class BookListView(generic.ListView):
@@ -24,3 +26,8 @@ class BookUpdateView(generic.UpdateView):
     model = Book
     fields = ['title', 'author', 'description', 'price']
     template_name = 'books/book_update.html'
+
+class BookDeleteView(generic.DeleteView):
+    model = Book
+    template_name = 'books/book_delete.html'
+    success_url = reverse_lazy('book_list')
