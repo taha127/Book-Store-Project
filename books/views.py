f6f0e18dcc5ee0e5ccb2b6451c6ac1fe21f4e693
taha127 <tahadehghani127@gmail.com>
2025-03-27 23:46:27 +0330

    refactor book detail view to use function-based approach and display comments

--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
 
 from .models import Book
 
@@ -13,9 +14,14 @@ class BookListView(generic.ListView):
     context_object_name = 'books'
 
 
-class BookDetailView(generic.DetailView):
-    model = Book
-    template_name = 'books/book_detail.html'
+# class BookDetailView(generic.DetailView):
+#     model = Book
+#     template_name = 'books/book_detail.html'
+def book_detail_view(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    # get the comments for the book
+    comments = book.comments.all()
+    return render(request, 'books/book_detail.html', {'book': book, 'comments': comments})
 
 
 class BookCreateView(generic.CreateView):
